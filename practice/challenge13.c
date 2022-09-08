2b78cead2961028d0f28a1e38c0ec4cb6f782c1e
soufianeargane <soufianeargane800@gmail.com>
2022-09-08 16:08:36 +0100

    challenge13

---
+++ practice/challenge13.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+# include <stdlib.h>
+int main(){
+	int num;
+	scanf("%d", &num);
+	printf("le nombre en octal %o \n", num);
+	printf("le nombre en hexadecimal %X", num);
+	return 0;
+}
